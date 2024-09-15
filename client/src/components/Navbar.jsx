c8bd7764858d5efff698ce9e90343e02363ebc73
Adrija <taniagori.001@gmail.com>
2024-09-16 02:09:50 +0530

    Dark mode added. Also lower navbar added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,31 @@
 import React from 'react'
-
-
+import { IoSearchOutline } from "react-icons/io5";
+import { IoMdCart } from "react-icons/io";
+import DarkMode from './DarkMode';
+import { FaAngleDown } from "react-icons/fa6";
+const Menu = [
+    {
+      id: 1,
+      name: "Home",
+      link: "/#",
+    },
+    {
+      id: 2,
+      name: "About Us",
+      link: "/#services",
+    },
+    {
+      id: 3,
+      name: "Know our Artiest",
+      link: "/#",
+    },
+    {
+      id: 3,
+      name: "Blog",
+      link: "/#",
+    },
+    
+  ];
 const Navbar = () => {
   return (
     <div className='shawdow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
@@ -15,15 +40,47 @@ const Navbar = () => {
                 </a>
             </div>
             {/* search bar and order button */}
-            <div>
-                <div className='group'>
-                    <input type="text"placeholder='search'className='w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-orange-400' />
+            <div className='flex justify-between items-center gap-4 '>
+                <div className=' relative group hidden sm:block'>
+                    <input type="text"placeholder='Search Here...'className='w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-orange-400' />
+                    <IoSearchOutline  className='text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3'/>
                 </div>
+            </div>
+            {/* order button  */}
+            <button onClick={()=> alert ("order not avaliable yet")} className='bg-gradient-to-r bg-orange-400 to bg-orange-600 transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group'>
+                <span className='group-hover:block hidden transition-all duration-200'>Order</span>
+                <IoMdCart className='text-xl text-white drop-shadow-sm cursor-pointer '/>
+            </button>
+            {/* darkmode Switch */}
+            <div>
+              <DarkMode />  
             </div>
         </div>
       </div>
       {/* lower Navbar */}
-      <div></div>
+      <div className="flex justify-center"> 
+        <ul className="sm:flex hidden items-center gap-4">
+            {
+                Menu.map((data)=>(
+                    <li key={data.id}>
+                        <a href={data.link} 
+                        className='inline-block px-4 hover:text-orange-400 duration-200'>{data.name}</a>
+                    </li>
+                )
+                
+                )
+            }
+            <li>
+                <a href="#" className="flex items-center gap-[2px] py-2">Categories
+                <span>
+                <FaAngleDown className="transition-all duration-200 group-hover:rotate-180"/>
+                </span>
+                </a>
+            </li>
+
+
+        </ul>
+      </div>
     </div>
   )
 }
