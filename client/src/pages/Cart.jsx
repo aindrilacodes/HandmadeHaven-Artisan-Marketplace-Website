0695fed4ace340074367cf751314781b52728768
Adrija <taniagori.001@gmail.com>
2024-10-23 00:54:19 +0530

    few changes are made

---
+++ client/src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>
+      <h1 className='text-gray-600'>cart page</h1>
+    </div>
+  )
+}
+
+export default Cart
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      <h1 className='bg-emerald-900'>Register page</h1>
+      
+    </div>
+  )
+}
+
+export default Register
