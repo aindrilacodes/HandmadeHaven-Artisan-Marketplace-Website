ec35ea69b8b35a7ba13cbee30d6c444a973c72d8
duttaaindrila23 <aindriladutta392@gmail.com>
2024-10-23 00:52:33 +0530

    made a few changes in userSchema

--- Server/src/models/userModel.js
+++ Server/src/models/userModel.js
@@ -16,14 +16,14 @@ const userSchema=new mongoose.Schema({
         trim:true,
         lowercase:true,
         match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
-        minlength:[3,'length of username too short'],
+        minlength:[5,'length of email too short'],
         maxlength:[31,'User email exceeded(max-31)']
     },
     password:{
         type:String,
         required:[true,'User password is required'],
         minlength:[8,'minimum size of 8 is required'],
-        maxlength:[12,'User name exceeded(max-12)'] 
+       // maxlength:[20,'User name exceeded(max-20)'] 
     },
     image:{
         type:String,
@@ -31,9 +31,10 @@ const userSchema=new mongoose.Schema({
     },
     Phone:{
         type:String,
-        required:[true,'User phone is required'], 
+        //required:[true,'User phone is required'], 
     },
 },{timestamps:true});
+
 
 
 const user=mongoose.model('users',userSchema)
