285842464480c6a69df8c0669838f13a96c2e4c2
duttaaindrila23 <aindriladutta392@gmail.com>
2024-10-23 00:53:01 +0530

    added the new routes

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,16 +1,19 @@
 const express=require('express');
 const morgan=require('morgan');
 const app=express();
-const router=require('./routers/authRoute')
+const cors = require('cors');
+const authrouter=require('./routers/authRoute')
 
 
 //middlewares
 app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json())
 
 
+
 //routes
-app.use("/api/v1/auth",router)
+app.use('/api/auth',authrouter);
 
 app.get("/test",(req,res)=>{
     res.status(200).send({message:"API is working just Fine"})
