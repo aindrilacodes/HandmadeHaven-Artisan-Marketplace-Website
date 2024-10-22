f32a628f7689da5ccb2133328f4ca20980434264
duttaaindrila23 <aindriladutta392@gmail.com>
2024-10-23 00:41:32 +0530

    changed envstore

--- Server/src/config/envstore.js
+++ Server/src/config/envstore.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const dotenv=require('dotenv')
+dotenv.config()
 const serverport=process.env.SERVER_PORT || 8002;
 const mongodburl=process.env.MONGODB_ATLAS_URL || 'mongodb://localhost:27017/HandmadeHaven';
 const defaultimagepath= process.env.DEFAULT_USER_IMAGE_PATH || 'Public/images/users/default.jpg';
