c6af99e486a7f8e9ecc8fdc2d284c066976c3b04
duttaaindrila23 <aindriladutta392@gmail.com>
2024-10-23 03:21:35 +0530

    changed the api

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -42,8 +42,6 @@ const Login = () => {
         alert("An error occurred while trying to login.");
       }
     };
-  
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-md rounded-lg">
